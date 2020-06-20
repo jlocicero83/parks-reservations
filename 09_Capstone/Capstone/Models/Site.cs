e9be59d093256f2afb4c85946675bd7836572774
Joe LoCicero <jlocicero83@gmail.com>
2020-06-20 16:29:28 -0400

    Added dictionary for True/False responses

--- 09_Capstone/Capstone/Models/Site.cs
+++ 09_Capstone/Capstone/Models/Site.cs
@@ -14,6 +14,18 @@ namespace Capstone.Models
         public int Max_RV_Length { get; set; }
         public bool HasUtilities { get; set; }
 
+        Dictionary<bool, string> accessible = new Dictionary<bool, string>()
+        {
+            {true, "Yes"},
+            {false, "No"}
+        };
+
+        Dictionary<bool, string> utilities = new Dictionary<bool, string>()
+        {
+            {true, "Yes"},
+            {false, "N/A"}
+        };
+
         public static string PrintHeader()
         {
             return $"\n\nResults Matching Your Search Criteria\n" +
@@ -25,7 +37,7 @@ namespace Capstone.Models
         public override string ToString()
         {
             return $"#{Site_Number, -9}{Max_Occupancy,-20}" +
-                $"{IsAccessible,-20}{Max_RV_Length,-20}{HasUtilities, -20}";
+                $"{accessible[IsAccessible],-20}{Max_RV_Length,-20}{utilities[HasUtilities], -20}";
         }
 
     }
